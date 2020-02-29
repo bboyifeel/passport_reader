c4a0e24eb300f98a7a21c78637b57a7a16ff0c04
Igor <bboyifeel@gmail.com>
2020-02-29 15:32:24 +0100

    + logging to the phone

--- app/src/main/java/com/lu/uni/igorzfeel/passport_reader/LoggingActivity.kt
+++ app/src/main/java/com/lu/uni/igorzfeel/passport_reader/LoggingActivity.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_logging.*
 import net.sf.scuba.smartcards.CardService
 import org.jmrtd.BACKey
 import org.jmrtd.PACEKeySpec
@@ -18,6 +19,7 @@ import org.jmrtd.lds.LDSFileUtil
 import org.jmrtd.lds.PACEInfo
 import org.jmrtd.lds.icao.DG1File
 import java.io.InputStream
+
 
 class LoggingActivity : AppCompatActivity() {
 
@@ -31,6 +33,8 @@ class LoggingActivity : AppCompatActivity() {
     private var birthDate: String = "000101"
     private var can: String? = null
 
+    private var log: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_logging)
@@ -40,14 +44,14 @@ class LoggingActivity : AppCompatActivity() {
         extractBundle(passportBundle)
 
         var nfcAdapter = NfcAdapter.getDefaultAdapter(this)
-        Log.d(TAG,"NFC supported ${(nfcAdapter != null).toString()}")
-        Log.d(TAG, "NFC enabled ${(nfcAdapter?.isEnabled).toString()}")
+        updateLog("NFC supported ${(nfcAdapter != null).toString()}")
+        updateLog("NFC enabled ${(nfcAdapter?.isEnabled).toString()}")
     }
 
     private fun extractBundle(bundle: Bundle) {
         can = bundle.getString("can")
-        Log.d(TAG,"Bundle has been extracted")
-        Log.d(TAG,"CAN " + can)
+        updateLog("Bundle has been extracted")
+        updateLog("CAN " + can)
     }
 
     override fun onResume() {
@@ -90,29 +94,35 @@ class LoggingActivity : AppCompatActivity() {
         if(intent == null)
             return
         if(NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.action)) {
-            Log.d(TAG, "NFC card has been discovered")
+            updateLog("NFC card has been discovered")
             var tag: Tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG)
             if (tag.techList.toList().contains("android.nfc.tech.IsoDep")) {
-                Log.d(TAG, "This is Iso supported tag")
+                updateLog("This is Iso supported tag")
                 readPassport(IsoDep.get(tag))
             }
             else {
-                Log.d(TAG, "I don't know this card")
+                updateLog("I don't know this card")
             }
         }
     }
 
+    private fun updateLog(msg: String) {
+        Log.d(TAG, msg)
+        log += msg + "\n"
+        logging_txtview_log.text = log
+    }
+
     private fun readPassport(isoDep: IsoDep) {
-        Log.d(TAG, "Let's read that passport")
+        updateLog("Let's read that passport")
 
         if (passportNumber.isNotEmpty()
             && expirationDate.isNotEmpty()
             && birthDate.isNotEmpty()
             && can!!.isNotEmpty()) {
-            Log.d(TAG, "Fields aren't empty")
+            updateLog("Fields aren't empty")
         }
         else {
-            Log.d(TAG, "[ERROR] Fields are empty")
+            updateLog("[ERROR] Fields are empty")
             Toast.makeText(this, "Empty field is not allowed", Toast.LENGTH_SHORT).show()
             return
         }
@@ -135,7 +145,7 @@ class LoggingActivity : AppCompatActivity() {
             try {
                 val cardAccessFile = CardAccessFile(passportService.getInputStream(PassportService.EF_CARD_ACCESS))
                 val secInfos = cardAccessFile.securityInfos
-                Log.d(TAG, cardAccessFile.toString())
+                updateLog(cardAccessFile.toString())
 
                 if (secInfos != null && secInfos.isNotEmpty()) {
                     Log.d(TAG, "PACE info has been found")
@@ -144,35 +154,35 @@ class LoggingActivity : AppCompatActivity() {
                     val paramId = paceInfo.parameterId
                     val params = PACEInfo.toParameterSpec(paramId)
 
-                    Log.d(TAG, paceInfo.protocolOIDString)
-                    Log.d(TAG, PACEInfo.toStandardizedParamIdString(paramId))
+                    updateLog(paceInfo.protocolOIDString)
+                    updateLog(PACEInfo.toStandardizedParamIdString(paramId))
 
                     passportService.doPACE(paceKey, oid, params, paramId)
                     paceSucceeded = true
-                    Log.d(TAG, "PACE has succeeded")
+                    updateLog("PACE has succeeded")
                 }
             } catch (e: Exception) {
-                Log.d(TAG, "PACE has failed with next error:")
-                Log.d(TAG, e.toString())
+                updateLog("PACE has failed with next error:")
+                updateLog(e.toString())
             }
 
             passportService.sendSelectApplet(paceSucceeded)
 
             if (!paceSucceeded) {
-                Log.d(TAG, "Let's try out to proceed with BAC")
+                updateLog("Let's try out to proceed with BAC")
                 passportService.doBAC(bacKey)
-                Log.d(TAG, "BAC success")
+                updateLog("BAC success")
             }
 
             var inputStream: InputStream = passportService.getInputStream(PassportService.EF_DG1)
             val dg1 = LDSFileUtil.getLDSFile(PassportService.EF_DG1, inputStream) as DG1File
-            Log.d(TAG, dg1.mrzInfo.nationality)
-            Log.d(TAG, dg1.mrzInfo.documentNumber)
-            Log.d(TAG, dg1.mrzInfo.dateOfExpiry)
-            Log.d(TAG, dg1.mrzInfo.gender.toString())
+            updateLog(dg1.mrzInfo.nationality)
+            updateLog(dg1.mrzInfo.documentNumber)
+            updateLog(dg1.mrzInfo.dateOfExpiry)
+            updateLog(dg1.mrzInfo.gender.toString())
 
         } catch (e: Exception) {
-            Log.d(TAG, e.toString())
+            updateLog(e.toString())
             Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show()
         }
     }
